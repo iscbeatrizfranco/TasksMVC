392c918a5ab7d13f4664671eb60163277e5ebdb8
Alejandra Franco <isc.alejandrafranco@gmail.com>
2025-06-25 11:40:51 -0700

    feature: install Entity Framework Core and configure the database connection string

---
+++ TasksMVC/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TasksMVC
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ApplicationDbContext()
+        {
+        }
+    }
+}
--- TasksMVC/Program.cs
+++ TasksMVC/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TasksMVC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
@@ -12,7 +18,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseRouting();
