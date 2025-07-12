7238994b1edcd90f0967166f336ef9258f8c1be0
Alejandra Franco <isc.alejandrafranco@gmail.com>
2025-07-12 09:59:30 -0700

    feature: create migration for add admin rol

--- TasksMVC/Migrations/20250712164448_AdminRol.Designer.cs
+++ TasksMVC/Migrations/20250712164448_AdminRol.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TasksMVC;
 
@@ -11,9 +12,11 @@ using TasksMVC;
 namespace TasksMVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250712164448_AdminRol")]
+    partial class AdminRol
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TasksMVC/Migrations/20250712164448_AdminRol.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TasksMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+IF NOT EXISTS(SELECT Id FROM AspNetRoles WHERE Id='0c1eddfe-c656-43b8-beb7-90a901d2b602')
+BEGIN
+	INSERT AspNetRoles (Id,[Name],[NormalizedName])
+	VALUES ('0c1eddfe-c656-43b8-beb7-90a901d2b602','admin','ADMIN');
+END
+");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE AspNetRoles WHERE Id = '0c1eddfe-c656-43b8-beb7-90a901d2b602'");
+        }
+    }
+}
